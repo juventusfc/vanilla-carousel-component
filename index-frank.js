34d455e214f92afcb8acf12858af5f23c465cb69
FrankHu <frank.hu@perficient.com>
2020-08-21 13:35:42 +0800

    add

--- index-frank.js
+++ index-frank.js
@@ -14,5 +14,35 @@ export default class Carousel {
 
       this.rootDom.appendChild(catImg);
     }
+
+    // add slide
+    let currentPosition = 0;
+    const slides = () => {
+      let nextPosition = (currentPosition + 1) % this.data.length;
+
+      let currentImg = this.rootDom.childNodes[currentPosition];
+      let nextImg = this.rootDom.childNodes[nextPosition];
+
+      currentImg.style.transition = `ease 0s`;
+      nextImg.style.transition = `ease 0s`;
+      currentImg.style.transform = `translateX(${-100 * currentPosition}%)`;
+      nextImg.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
+
+      setTimeout(() => {
+        currentImg.style.transition = `transform ease 0.5s`;
+        nextImg.style.transition = `transform ease 0.5s`;
+        currentImg.style.transform = `translateX(${
+          -100 * (currentPosition + 1)
+        }%)`;
+        nextImg.style.transform = `translateX(${
+          100 - 100 * (nextPosition + 1)
+        }%)`;
+
+        currentPosition = nextPosition;
+      }, 16);
+
+      setTimeout(slides, 3000);
+    };
+    setTimeout(slides, 3000);
   }
 }
